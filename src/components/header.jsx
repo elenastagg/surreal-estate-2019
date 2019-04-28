b523ae490b87651a6c19b521ea9ef71ecfff120f
Elena Stagg <elenagracestagg@hotmail.co.uk>
2019-04-28 21:33:14 +0630

    updated logo image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import logo from '../styles/stagoutline.png';
 import '../styles/header.css';
 
 const Header = () => (
   <div className="header-image">
     <header className="header">
-      <img className="logo" src="https://i.pinimg.com/originals/1c/d6/22/1cd6226b62ebbd9e41dab207ae1eca6b.png" />
+      <img className="logo" src={logo} />
       <nav className="nav">
         <Link className="nav-item" to="/">Property Listings</Link>
         <Link className="nav-item" to="/add-property">Add Property</Link>
