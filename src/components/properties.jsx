f9ebe476092955ec63ff34fdb408e9328e21ecf6
Elena Stagg <elenagracestagg@hotmail.co.uk>
2019-05-17 12:22:27 +0630

    added a search query to filter properties in each location tab

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -7,6 +7,8 @@ import PropertyCard from './property-card';
 import '../styles/main-content-box.css';
 import '../styles/location-nav.css';
 
+const cities = ['Manchester', 'Leeds', 'Sheffield', 'Liverpool'];
+
 class Properties extends React.Component {
   constructor(props) {
     super(props);
@@ -32,16 +34,32 @@ class Properties extends React.Component {
       });
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.search !== this.props.location.search) {
+      axios.get(`http://localhost:3000/api/v1/PropertyListing${this.props.location.search }`)
+        .then((res) => {
+          this.setState({
+            properties: res.data,
+          });
+        })
+        .catch(() => {
+          this.setState({
+            isError: true,
+            alertMessage: 'There was a problem retrieving the properties',
+          });
+        });
+    }
+  }
+
   render() {
     return (
       <Fragment>
         <div className="page properties-page">
           <span className="title">Property Listings</span>
           <div className="location-nav">
-            <Link className="tab" to="/properties?sort=Manchester">Manchester</Link>
-            <Link className="tab" to="/properties?sort=Leeds">Leeds</Link>
-            <Link className="tab" to="/properties?sort=Sheffield">Sheffield</Link>
-            <Link className="tab" to="/properties?sort=Liverpool">Liverpool</Link>
+            {cities.map(city => (
+              <Link key={city} className="tab" to={`/?query={ "city": "${city}" }`}>{city}</Link>
+            ))}
           </div>
           <div className="main-content-box">
             {this.state.properties.map(property => (
