c7267d982c6ab6ba9fc797cbef94a4ff00100903
Elena Stagg <elenagracestagg@hotmail.co.uk>
2019-05-17 16:21:24 +0630

    added filtering and sorting functionality for property listings

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import '../styles/page-titles.css';
 import axios from 'axios';
 import Alert from './alert';
+import qs from 'qs';
 import { Link } from 'react-router-dom';
 import PropertyCard from './property-card';
 import '../styles/main-content-box.css';
@@ -51,15 +52,28 @@ class Properties extends React.Component {
     }
   }
 
+  buildQueryString(operation, valueObj) {
+    const search = this.props.location.search;
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true }); // turn into object
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj), // add object with stringified value
+    };
+    return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+  }
+
   render() {
     return (
       <Fragment>
         <div className="page properties-page">
-          <span className="title">Property Listings</span>
+          <div className="title">Property Listings</div>
           <div className="location-nav">
+            <Link className="sort-function tab" to={this.buildQueryString('sort', { price: -1 })}>Sort: Price Descending</Link>
+            <Link className="sort-function tab" to={this.buildQueryString('sort', { price: 1 })}>Sort: Price Ascending</Link>
             {cities.map(city => (
-              <Link key={city} className="tab" to={`/?query={ "city": "${city}" }`}>{city}</Link>
+              <Link key={city} className="tab" to={this.buildQueryString('query', { city: `${city}` })}>{city}</Link>
             ))}
+            <Link className="sort-function tab" to={this.buildQueryString('sort', { price: -1 })}>Sort: Price Descending</Link>
           </div>
           <div className="main-content-box">
             {this.state.properties.map(property => (
